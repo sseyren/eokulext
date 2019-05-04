6c807db0dec3a3e2fe884b2f31c581fbc4874b47
thesseyren <sseyren@hotmail.com>
2019-05-04 14:10:01 +0300

    browserify bağımlılıkları koda gömüldü

--- src/scripts/ayarlar.js
+++ src/scripts/ayarlar.js
@@ -1,4 +1,20 @@
-import storage from "./utils/storage";
+const _apis = [ 'alarms', 'bookmarks', 'browserAction', 'commands', 'contextMenus', 'cookies', 'downloads', 'events', 'extension', 'extensionTypes', 'history', 'i18n', 'idle', 'notifications', 'pageAction', 'runtime', 'storage', 'tabs', 'webNavigation', 'webRequest', 'windows' ]
+
+function _Extension () {
+    const _this = this
+    _apis.forEach(function (api) {
+        _this[api] = null
+        try { if (chrome[api]) { _this[api] = chrome[api] } } catch (e) {}
+        try { if (window[api]) { _this[api] = window[api] } } catch (e) {}
+        try { if (browser[api]) { _this[api] = browser[api] } } catch (e) {}
+        try { _this.api = browser.extension[api] } catch (e) {}
+    })
+    try { if (browser && browser.runtime) { this.runtime = browser.runtime } } catch (e) {}
+    try { if (browser && browser.browserAction) { this.browserAction = browser.browserAction } } catch (e) {}
+}
+
+const ext = new _Extension();
+const storage = (ext.storage.sync ? ext.storage.sync : ext.storage.local);
 
 let varsayilanlar = {
     gecmeNot: 50,
